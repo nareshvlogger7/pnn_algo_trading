f91a938a225968d70a5f4d6608b29689c04d0426
nareshvlogger7 <166097147+nareshvlogger7@users.noreply.github.com>
2024-10-20 20:45:42 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config(); 
-const mysql = require("mysql2");
-const urlBD= mysql.createConnection//root:oYxaHDcQlCVRGsWIStqxnHwmhjJQhNzH@mysql.railway.internal:3306/railway
-const connection = mysql.createConnection(urlBD);
-module.exports=connection;
